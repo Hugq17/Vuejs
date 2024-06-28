854a607c913a930f645164a2029ab132ff87f4da
hung1720 <coderlazy17@gmail.com>
2024-06-28 23:31:31 +0700

    Day3-reduce

---
+++ 02-syntax/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const message = "hellow world";
+const rawHtml = "<h1>How are you today</h1>"; // raw HTML
+const buttonId = "button-id";
+const buttonClass = "button-class";
+</script>
+
+<template>
+  <h1>Syntax</h1>
+  <p>Message: {{ message }}</p>
+  <!-- sử dụng 2 cặp {{}} để truyền biến vào-->
+  <span v-html="rawHtml"></span>
+  <button :id="buttonId" :class="buttonClass">Click me</button>
+  <!-- sử dụng : Ràng buộc thuộc tính-->
+</template>
