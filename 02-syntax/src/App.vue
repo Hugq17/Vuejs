8b2412cd445b3b9f729699e1b0fac701f755a261
hung1720 <coderlazy17@gmail.com>
2024-07-04 12:23:07 +0700

    update 2-syntax

--- 02-syntax/src/App.vue
+++ 02-syntax/src/App.vue
@@ -3,6 +3,18 @@ const message = "hellow world";
 const rawHtml = "<h1>How are you today</h1>"; // raw HTML
 const buttonId = "button-id";
 const buttonClass = "button-class";
+const rawHtml2 = "<h1>Hello</h1>";
+const buttonId1 = "buttonID1";
+const buttonClass1 = "buttonCLASS1";
+const isButtonDisabled = true;
+//  vô hiệu hóa button
+const objectOfAttrs = {
+  id: "container",
+  class: "wrapper",
+  style: {
+    color: "pink",
+  },
+};
 </script>
 
 <template>
@@ -12,4 +24,10 @@ const buttonClass = "button-class";
   <span v-html="rawHtml"></span>
   <button :id="buttonId" :class="buttonClass">Click me</button>
   <!-- sử dụng : Ràng buộc thuộc tính-->
+  <span v-html="rawHtml2"></span>
+  <button :id="buttonId1" :class="buttonClass1" :disabled="isButtonDisabled">
+    Click me
+  </button>
+  <!-- vô hiệu hóa button -->
+  <button v-bind="objectOfAttrs">Click here !!!</button>
 </template>
